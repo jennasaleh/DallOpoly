63c90b8acc27a0727079617ca57562de6a625293
drbortel <drbortel@gmail.com>
2015-09-01 19:41:59 -0500

    test

--- test/main/PropertyTests.java
+++ test/main/PropertyTests.java
@@ -26,7 +26,7 @@ public class PropertyTests {
 			Tile currentPlayerTile = new Tile(200,0);
 			boolean result = currentPlayerTile.buy(currentPlayer);
 			Assert.assertTrue("Result was " + result, result);
-		} catch (TooLittleMoney e) {
+		} catch (PropertyBuyNoFunds e) {
 			fail(e.getLocalizedMessage());
 		}
 		
@@ -35,9 +35,49 @@ public class PropertyTests {
 	@Test
 	public void testFailToBuyNotAvailable() {
 		try {
-			boolean result = currentPlayerTile.buy(currentPlayer);
-			Assert.assertTrue("Result was " + result, result);
+			boolean result = currentPlayerTile.buy(currentPlayer,OwnedBy!=p);
+			Assert.assertTrue("Bought but Not Enough Money: " + result, result);
 		} catch (PropertyNotAvailable e) {
+			fail(e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void testFailToBuyAlreadyOwned() {
+		try {
+			boolean result = currentPlayerTile.buy(currentPlayer,OwnedBy=p);
+			Assert.assertTrue("Bought but Already Owned: " + result, result);
+		} catch (PropertyBuyDuplicate e) {
+			fail(e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void testLimit40Tiles() {
+		try {
+			boolean result = numTiles > 40;
+			Assert.assertTrue("Tiles More than 40: " + result, result);
+		} catch (TilesLimitExceeded e) {
+			fail(e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void testLimitMoneyBelowZero() {
+		try {
+			boolean result = money < 0;
+			Assert.assertTrue("Money Less than Zero: " + result, result);
+		} catch (MoneyNotAvailable e) {
+			fail(e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void testLimit4Houses() {
+		try {
+			boolean result = houses > 4;
+			Assert.assertTrue("More than 4 Houses: " + result, result);
+		} catch (HouseNotAvailable e) {
 			fail(e.getLocalizedMessage());
 		}
 	}
