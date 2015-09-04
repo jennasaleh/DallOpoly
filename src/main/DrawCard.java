c2111206f7a33fa0898317d706d69f0442ab6b59
drbortel <drbortel@gmail.com>
2015-09-04 09:28:54 -0500

    Amateras UML Plugin

---
+++ src/main/DrawCard.java
@@ -0,0 +1,10 @@
+package main;
+
+public class DrawCard extends Tile {
+
+	public DrawCard(int cost, int id) {
+		super(cost, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/Go.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Go extends Tile {
+
+	public Go(int cost, int id) {
+		super(cost, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
